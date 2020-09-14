54848f0f58f1c9905e13eaba78c3defbff69b9e6
Anindya Roy <aniroy@linkedin.biz>
2020-09-14 17:37:45 +0530

    NDTV - Remove unused imports from test class and added gitignore

--- src/test/java/com/qa/ndtv/test/weather/TemperatureTest.java
+++ src/test/java/com/qa/ndtv/test/weather/TemperatureTest.java
@@ -3,7 +3,6 @@ package com.qa.ndtv.test.weather;
 import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.util.List;
-import java.util.stream.Stream;
 
 import org.testng.Assert;
 import org.testng.Reporter;
